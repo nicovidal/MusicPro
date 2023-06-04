e1504c32784c127e53e635928fa6faa8a86c8a64
nicovidal <nico.vidal.m@gmail.com>
2023-06-04 19:21:39 -0400

    models cambiarlos

--- api/models.py
+++ api/models.py
@@ -3,4 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Producto(models.Model):
+    serie_del_producto=models.CharField(max_length=50)
+    marca=models.CharField(max_length=50)
+    codigo=models.CharField(max_length=50)
     nombre=models.CharField(max_length=50)
+    precio=models.CharField(max_length=50)
+    modelo=models.CharField(max_length=50)
+
+
